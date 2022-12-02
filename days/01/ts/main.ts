ed8a207e432b8789442c8989b74e71bf55e33e75
Joey Thies <joey.thies@gmail.com>
2022-12-01 16:50:54 -0800

    result with bst

--- days/01/ts/main.ts
+++ days/01/ts/main.ts
@@ -1,35 +1,85 @@
 import path from "node:path";
 import fs from "node:fs/promises";
 
+class BST {
+  root: Node | undefined;
+
+  constructor() {}
+  insert(value: number, meta?: Record<string, unknown>) {
+    let current: Node | undefined;
+    let x = this.root;
+    const next = new Node(value, meta);
+
+    while (x) {
+      current = x;
+      if (next.data < x.data) {
+        x = x.left;
+      } else {
+        x = x.right;
+      }
+    }
+
+    next.parent = current;
+    if (!current) {
+      this.root = next;
+    } else if (next.data < current.data) {
+      current.left = next;
+    } else {
+      current.right = next;
+    }
+  }
+}
+class Node {
+  data: number;
+  meta?: Record<string, unknown>;
+  parent?: Node;
+  left?: Node;
+  right?: Node;
+  constructor(value: number, meta?: Record<string, unknown>) {
+    this.data = value;
+    if (meta) {
+      this.meta = meta;
+    }
+  }
+}
+
 async function main() {
   const datapath = path.resolve(__dirname, "../input.txt");
   const file = await fs.open(datapath);
-  // const stream = file.createReadStream();
 
-  let elves: number[] = [];
-  let currentElfCalories = 0;
+  let elves = new BST();
+  let index: number = 0;
+  let calories = 0;
 
   for await (const chunk of file.readLines()) {
+    // empty line implies new elf
     if (!chunk) {
-      elves.push(currentElfCalories);
-      currentElfCalories = 0;
+      elves.insert(calories, { index });
+      calories = 0;
+      index += 1;
       continue;
     }
 
     let value = parseInt(chunk);
-    currentElfCalories += value;
+    calories += value;
   }
 
-  elves.sort((a, b) => b - a);
-
-  const topThree = elves.slice(0, 3);
-  return topThree.reduce((acc, curr) => {
-    acc += curr;
-    return acc;
-  }, 0);
+  let total = 0;
+  let t = 3;
+  function walk(node?: Node) {
+    if (node) {
+      walk(node.right);
+      if (t > 0) {
+        total += node.data;
+      }
+      t = t - 1;
+      walk(node.left);
+    }
+  }
+  walk(elves.root);
+  return total;
 }
 
 main().then((total) => {
-  console.log(total);
-  // console.log(`Maximum: ${max} at ${idx}`);
+  console.log(`top three elves total calories: ${total}`);
 });
