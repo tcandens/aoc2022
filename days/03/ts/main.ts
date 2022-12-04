4c874d2106903f3bc590d8b0d759aab7f660a194
Joey Thies <joey.thies@gmail.com>
2022-12-04 13:49:35 -0800

    working

--- days/03/ts/main.ts
+++ days/03/ts/main.ts
@@ -1,40 +1,55 @@
 import fs from "node:fs/promises";
 import path from "node:path";
 
-let priorities = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const group_size = 3;
+
+function utf8_byte_to_priority(byte: number) {
+  if (byte < 91) {
+    return byte - 38;
+  } else {
+    return byte - 96;
+  }
+}
 
 async function main() {
   const datapath = path.resolve(__dirname, "input.txt");
   const file = await fs.open(datapath);
 
-  let duplicates = "";
+  let group_totals = 0;
+  let group_bytes = new Map();
+  let line_number = 0;
 
   for await (let line of file.readLines()) {
     line = line.trim();
-    let firstMap = new Map();
-    let lineDupes = "";
+    let group_index = line_number % group_size;
+
     for (let i = 0; i < line.length; i++) {
-      let char = line.charAt(i);
-      if (i < line.length / 2) {
-        firstMap.set(char, true);
-      } else {
-        let found = firstMap.has(char);
-        if (found && lineDupes.indexOf(char) == -1) {
-          lineDupes = lineDupes.concat(char);
-        }
+      let byte = line.charCodeAt(i);
+      let current = group_bytes.get(byte);
+      if (!current) {
+        group_bytes.set(byte, [group_index]);
+      } else if (Array.isArray(current) && !current.includes(group_index)) {
+        group_bytes.set(byte, [...current, group_index]);
       }
     }
-    duplicates = duplicates.concat(lineDupes);
+
+    if (line_number > 0 && group_index === group_size - 1) {
+      // last line of three person group let check group bytes
+      for (let [byte, state] of group_bytes.entries()) {
+        if (Array.isArray(state) && state.length === group_size) {
+          let priority = utf8_byte_to_priority(byte);
+          group_totals += priority;
+        }
+      }
+
+      // reset groups
+      group_bytes = new Map();
+    }
+
+    line_number++;
   }
 
-  const total = duplicates.split("").reduce((acc, curr) => {
-    let val = priorities.indexOf(curr) + 1;
-    // console.log(`dupe: ${curr}; val: ${val}`);
-    acc += val;
-    return acc;
-  }, 0);
-
-  return total;
+  return group_totals;
 }
 
 main().then((total) => console.log(`priority totals: ${total}`));
